96832a1e528e6629d6a983bd44e836957e7dbf9f
Harrison Pan <hanshen36@gmail.com>
2022-06-13 22:51:01 +1200

    Add Index controller and index template

---
+++ src/main/java/com/example/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Harrison Pan
+ */
+@Controller
+public class IndexController {
+
+  @RequestMapping({"", "/", "/index"})
+  public String getIndexPage() {
+    return "index";
+  }
+}
