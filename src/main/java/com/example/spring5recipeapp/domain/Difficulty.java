c9c8b6b1c56fbeb0095a6b3395c2d61079b2daad
Harrison Pan <hanshen36@gmail.com>
2022-07-30 22:22:03 +1200

    feat: 🎸 Add enum Difficulity

---
+++ src/main/java/com/example/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,10 @@
+package com.example.spring5recipeapp.domain;
+
+/**
+ * @author Harrison Pan
+ */
+
+public enum Difficulty {
+  //  Enum for the difficulty of the recipe
+  EASY, MODERATE, HARD
+}
